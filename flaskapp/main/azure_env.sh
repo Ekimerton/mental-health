c0249819cb5c6c0a420ea139e1bfcd63ba9ec12b
alexshi0000 <yuanshi948@gmail.com>
2019-09-14 17:47:53 -0400

    did nlp

--- flaskapp/main/azure_env.sh
+++ flaskapp/main/azure_env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export TEXT_ANALYTICS_SUBSCRIPTION_KEY="1e11338c89f44569a235098fc8243663"
-export TEXT_ANALYTICS_ENDPOINT="https://mental-health-app.cognitiveservices.azure.com/luis/v2.0"
+export TEXT_ANALYTICS_SUBSCRIPTION_KEY="cc44c6b9c0c14a3a83cff47db0bb6e3c"
+export TEXT_ANALYTICS_ENDPOINT="https://eastus.api.cognitive.microsoft.com/text/analytics/v2.1/sentiment?showStats=false"
--- flaskapp/main/nlp.py
+++ flaskapp/main/nlp.py
@@ -1,34 +1,37 @@
-from azure.cognitiveservices.language.textanalytics import TextAnalyticsClient
-from msrest.authentication import CognitiveServicesCredentials
-
 import os
+import requests
+
+# you are going to have to run source azure_env.sh
 
 key_var_name = 'TEXT_ANALYTICS_SUBSCRIPTION_KEY'
 if not key_var_name in os.environ:
-    raise Exception('Please set/export the environment variable: {}'.format(key_var_name))
+    raise Exception('Please set/export the environment variable: '.format(key_var_name))
 subscription_key = os.environ[key_var_name]
 
 endpoint_var_name = 'TEXT_ANALYTICS_ENDPOINT'
 if not endpoint_var_name in os.environ:
-    raise Exception('Please set/export the environment variable: {}'.format(endpoint_var_name))
+    raise Exception('Please set/export the environment variable: '.format(endpoint_var_name))
 endpoint = os.environ[endpoint_var_name]
 
-credentials = CognitiveServicesCredentials(subscription_key)
-print("credentials:" + str(credentials))
+print("subscription-key: " + subscription_key)
+print("endpoint: " + endpoint)
 
-# choose request url and also
-endpoint = "https://" + endpoint_var_name + "/text/analytics/v2.1/sentiment?showStats=true"
-text_analytics = TextAnalyticsClient(endpoint=endpoint, credentials=credentials)
-
-# sentimental analysis
-documents = [
-    {
-        "id": "1",
-        "language": "en",
-        "text": "I had the best day of my life."
+def get_document(post):
+    data = {
+        "documents": [{
+            "id": "1", "language": "en",
+            "text": post
+        }]
     }
-]
-response = text_analytics.sentiment(documents=documents)
-for document in response.documents:
-    print("Document Id: ", document.id, ", Sentiment Score: ",
-          "{:.2f}".format(document.score))
+    return data
+
+def get_sentiment_score(document):
+    headers = {
+        "Ocp-Apim-Subscription-Key": subscription_key,
+        "Content-Type": "application/json"
+    }
+    response = requests.post(endpoint, headers=headers, json=document)
+    sentiments = response.json()
+    print(sentiments)
+
+# get_sentiment_score(get_document("i'm really happy"))
