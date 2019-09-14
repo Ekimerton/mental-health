05fcf3e263de3654e482485b11ad1b635968556b
Ekimerton <ekim0252@gmail.com>
2019-09-14 15:24:15 -0400

    Added user registration and login

--- flaskapp/models.py
+++ flaskapp/models.py
@@ -1,15 +1,16 @@
 from datetime import datetime
 from flask import current_app
 from flaskapp import db
+from flask_login import UserMixin
 
-class User:
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(15), unique=True, nullable=False)
+    username = db.Column(db.String(15), nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
-class Post:
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
