42f1291c455fdd7a80633524ad089f0ed154040b
Ekimerton <ekim0252@gmail.com>
2019-09-14 11:44:57 -0400

    fixed imports

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from webapp.config import Config
+from flaskapp.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from webapp.main.routes import main
-from webapp.errors.handlers import errors
+from flaskapp.main.routes import main
+from flaskapp.errors.handlers import errors
 app.register_blueprint(main)
 app.register_blueprint(errors)
