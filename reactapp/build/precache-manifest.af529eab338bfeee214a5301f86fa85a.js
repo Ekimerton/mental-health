375a3e2233d5cf4c6e486e61becac55c11bc2f28
alexshi0000 <yuanshi948@gmail.com>
2019-09-15 02:06:51 -0400

    redo ignore

---
+++ reactapp/build/precache-manifest.af529eab338bfeee214a5301f86fa85a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25693cbe0937430ddc6e074abe16ed3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86dd632ae3717f189fec",
+    "url": "/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "50c6f4a2b8f3aa7d22b5",
+    "url": "/static/css/main.db1d59c8.chunk.css"
+  },
+  {
+    "revision": "86dd632ae3717f189fec",
+    "url": "/static/js/2.e9828f13.chunk.js"
+  },
+  {
+    "revision": "50c6f4a2b8f3aa7d22b5",
+    "url": "/static/js/main.cde65ef2.chunk.js"
+  },
+  {
+    "revision": "36b61135f620480176d4",
+    "url": "/static/js/runtime~main.901fe662.js"
+  }
+]);
